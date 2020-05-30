138247fd757450be14cba6c79c12319be480012a
Artem Romanov <romanov-artyom@bk.ru>
2020-05-30 23:29:55 +0300

    Worker for xml file is ready

--- data_worker.py
+++ data_worker.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import json
+import xml.etree.ElementTree as ET
 
 
 def get_json_file_content():
@@ -7,7 +8,13 @@ def get_json_file_content():
         return json.load(json_file)
 
 
-def print_top_words_longer_six_chars():
+def get_xml_root_content():
+    with open('newsafr.xml', encoding='utf-8') as xml_file:
+        parser = ET.XMLParser(encoding='utf-8')
+        return ET.parse(xml_file, parser).getroot()
+
+
+def print_top_words_from_json_news():
     content = get_json_file_content()
     all_words_longer_six = []
     for item in content['rss']['channel']['items']:
@@ -18,11 +25,27 @@ def print_top_words_longer_six_chars():
             if len(word_in_title) > 6:
                 all_words_longer_six.append(word_in_title.lower())
     most_popular = Counter(all_words_longer_six).most_common(10)
-    print(most_popular)
-    print(type(most_popular))
+    print('\nНаиболее часто встречающиеся слова в описании новостей (from json):')
+    for pair in most_popular:
+        print(f'Слово - "{pair[0]}", раз встречается - {pair[1]}')
+
+
+def print_top_words_from_xml_news():
+    root = get_xml_root_content()
+    all_words_longer_six = []
+    for item in root.findall('channel/item'):
+        for word_in_desc in item.find('description').text.split():
+            if len(word_in_desc) > 6:
+                all_words_longer_six.append(word_in_desc.lower())
+        for word_in_title in item.find('title').text.split():
+            if len(word_in_title) > 6:
+                all_words_longer_six.append(word_in_title.lower())
+    most_popular = Counter(all_words_longer_six).most_common(10)
+    print('\nНаиболее часто встречающиеся слова в описании новостей (from xml):')
     for pair in most_popular:
         print(f'Слово - "{pair[0]}", раз встречается - {pair[1]}')
 
 
 if __name__ == '__main__':
-    print_top_words_longer_six_chars()
+    print_top_words_from_json_news()
+    print_top_words_from_xml_news()
